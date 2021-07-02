b68fe3708a45ced4a0157b1da13e37d4100453a9
Haliz <foril2n1@rambler.ru>
2021-07-02 21:01:52 +0300

    1.1. Job c параметрами

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,7 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
@@ -15,12 +16,16 @@ public class AlertRabbit {
     private static final Properties PROPERTIES = new Properties();
 
     public static void main(String[] args) {
-        try {
             AlertRabbit.loadProperties();
+        try (Connection connection = getConnection()) {
             int interval = Integer.parseInt(PROPERTIES.getProperty("rabbit.interval"));
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("cn", connection);
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder times = simpleSchedule()
                     .withIntervalInSeconds(interval)
                     .repeatForever();
@@ -29,7 +34,10 @@ public class AlertRabbit {
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+            System.out.println(" Здесь запись после завершения.");
+        } catch (Exception se) {
             se.printStackTrace();
         }
     }
@@ -43,11 +51,28 @@ public class AlertRabbit {
         }
     }
 
+    private static Connection getConnection() throws Exception {
+        Class.forName(PROPERTIES.getProperty("driver-class-name"));
+        return DriverManager.getConnection(
+                PROPERTIES.getProperty("url"),
+                PROPERTIES.getProperty("username"),
+                PROPERTIES.getProperty("password")
+        );
+    }
+
     public static class Rabbit implements Job {
 
         @Override
         public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("cn");
+            try (PreparedStatement statement = connection
+                    .prepareStatement(
+                            "insert into rabbit (created_date) values(current_timestamp);")) {
+                statement.execute();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
