ed90f2c4f067c59a1d5554fcf477b7350d7d0bf1
Haliz <foril2n1@rambler.ru>
2021-07-09 12:22:13 +0300

    2.4. SqlRuParse

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -48,4 +48,14 @@ public class Post {
     public void setCreated(LocalDateTime created) {
         this.created = created;
     }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created + '}';
+    }
 }
--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,33 +4,64 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
-public class SqlRuParse {
-    public static void main(String[] args) throws Exception {
-        for (int i = 1; i <= 5; i++) {
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Element href = td.child(0);
-                String link = href.attr("href");
-                System.out.println(link);
-                System.out.println(href.text());
-                Element date = td.parent().child(5);
-                System.out.println(date.text());
-                parsingPostDescription(link);
-            }
-        }
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SqlRuParse implements Parse {
+    public static void main(String[] args) {
+        SqlRuParse parse = new SqlRuParse();
+        List<Post> list = parse.list("https://www.sql.ru/forum/job-offers");
+        list.forEach(System.out::println);
+        System.out.println(list.size());
     }
 
-    public static void parsingPostDescription(String url) throws Exception {
-        Document doc = Jsoup.connect(url).get();
-        Elements row = doc.select(".msgBody");
-        String text = row.get(1).text();
-        String date = doc.select(".msgFooter").first().ownText();
-        int index = date.indexOf(" [] |");
-        String editedDate = date.substring(0, index);
-        System.out.println(text);
-        System.out.println(editedDate);
-        System.out.println("------------------------------------------------------------");
+    @Override
+    public List<Post> list(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            for (int i = 1; i <= 5; i++) {
+                Document doc = Jsoup.connect(link + '/' + i).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    Element href = td.child(0);
+                    String descriptionLink = href.attr("href");
+                    String title = href.text();
+                    Post post = detail(descriptionLink);
+                    post.setLink(descriptionLink);
+                    post.setTitle(title);
+                    posts.add(post);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+
+    @Override
+    public Post detail(String link) {
+        Post post = new Post();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".msgBody");
+            String text = row.get(1).text();
+            String date = doc.select(".msgFooter").first().ownText();
+            int index = date.indexOf(" [] |");
+            String editedDate = date.substring(0, index);
+            post.setDescription(text);
+            DateTimeParser dateParser = new SqlRuDateTimeParser();
+            LocalDateTime setDate = dateParser.parse(editedDate);
+            post.setCreated(setDate);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 }
