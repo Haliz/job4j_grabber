01deb1ab45213df95e1c1836b7ded4243a0de6c7
Haliz <foril2n1@rambler.ru>
2021-07-12 20:52:16 +0300

    6. Grabber

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -59,13 +59,13 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id == post.id && Objects.equals(title, post.title) && Objects.equals(link, post.link)
+        return Objects.equals(title, post.title) && Objects.equals(link, post.link)
                 && Objects.equals(created, post.created);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, created);
+        return Objects.hash(title, link, created);
     }
 
     @Override
